4a105bb3183a0d7ce34b172a3392f9e44f4d950f
Xavier <xavier.cucurull@upc.edu>
2021-12-09 10:16:07 +0100

    Update config file

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-PODCASTS_FILE = "data/apple_podcasts.csv"
-
-MAX_SHOWS_PER_SEARCH = 10
+"""
+CONFIGURATION FILE
+"""
 
 SPOTIPY_CLIENT_ID ='8c8189fe204c401fab8b265ce857a834'       # Your spotify Client ID
 SPOTIPY_CLIENT_SECRET ='4f31f73ce5934fafa4914d9a4b3d37bb'   # Your spotify Client Secret
+
+PODCASTS_FILE = "data/apple_podcasts.csv"
+
+MAX_SHOWS_PER_SEARCH = 10
+BATCH_SIZE = 100
+POOL_PROCESSES = 5
