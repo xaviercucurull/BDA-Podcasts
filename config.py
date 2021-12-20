7dc67490196c1d8f6ca2902b2ad925ce0876bd0b
xaviercucurull <xavierqq@gmail.com>
2021-12-20 16:11:07 +0100

    Hide spotify client keys

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 CONFIGURATION FILE
 """
 
-SPOTIPY_CLIENT_ID ='8c8189fe204c401fab8b265ce857a834'       # Your spotify Client ID
-SPOTIPY_CLIENT_SECRET ='4f31f73ce5934fafa4914d9a4b3d37bb'   # Your spotify Client Secret
+SPOTIPY_CLIENT_ID ='XXXXXXXXXXXXXXXXXXX'       # Your spotify Client ID
+SPOTIPY_CLIENT_SECRET ='XXXXXXXXXXXXXXXXXXX'   # Your spotify Client Secret
 
 PODCASTS_FILE = 'data/apple_podcasts.csv'
 
